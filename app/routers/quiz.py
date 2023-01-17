9c102c5bf0f9834c2fdfe853f5b9134da14a2f2e
AbdulRashidReshamwala <abdulrreshamwala@gmail.com>
2023-01-18 03:43:38 +0530

    added create quiz endpoint

--- app/routers/quiz.py
+++ app/routers/quiz.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from ..services.quiz import QuizService
+from ..models.quiz import CreateQuizInput
+from ..dependencies import get_quiz_service
 
 router = APIRouter(
     prefix="/quiz",
@@ -8,5 +11,9 @@ router = APIRouter(
 
 
 @router.post("/create")
-def ceate_quiz():
+def ceate_quiz(
+    create_quiz_input: CreateQuizInput,
+    quiz_service: QuizService = Depends(get_quiz_service),
+):
+    quiz_service.create_quiz(create_quiz_input)
     pass
