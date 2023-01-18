a599973b0fdaae4657968bb8ae6f536014de95a1
AbdulRashidReshamwala <abdulrreshamwala@gmail.com>
2023-01-19 02:55:55 +0530

    add per request logging

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
+import random
+import string
+import time
 from fastapi import FastAPI, Request
 from .routers import auth, quiz
 from .config import settings
 from .utils import setup_firebase
 from fastapi.templating import Jinja2Templates
+from loguru import logger
+
 
 templates = Jinja2Templates(directory="app/templates")
 
@@ -21,14 +26,31 @@ async def read_index(request: Request):
 
 @app.on_event("startup")
 async def startup_event():
-    print(f"Starting application in env : {settings.env}")
+    logger.info(f"Starting application in env : {settings.env}")
     setup_firebase()
-    print(f"Started application sucessfully")
+    logger.info(f"Started application sucessfully")
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    print("Shutting down application")
+    logger.info("Shutting down application")
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    idem = "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    logger.info(f"rid={idem} start request path={request.url.path}")
+    start_time = time.time()
+
+    response = await call_next(request)
+
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = "{0:.2f}".format(process_time)
+    logger.info(
+        f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}"
+    )
+
+    return response
 
 
 app.include_router(auth.router)
