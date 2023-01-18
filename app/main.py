e3b577d9c6f952ff98c26c7f1451cca37accc294
AbdulRashidReshamwala <abdulrreshamwala@gmail.com>
2023-01-18 16:42:38 +0530

    implement poc login ui

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from .routers import auth, quiz
 from .config import settings
 from .utils import setup_firebase
+from fastapi.templating import Jinja2Templates
 
+templates = Jinja2Templates(directory="app/templates")
 
 app = FastAPI()
 
 
-@app.get("/", tags=["health_check"], name="Health Check")
+@app.get("/health", tags=["health_check"], name="Health Check")
 def read_root() -> str:
     return "Hello World 2"
+
+
+@app.get("/")
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.on_event("startup")
